af7c96a5aec006eb00764cf0485ff3b2b6b52392
zachoines <zachoines@gmail.com>
2025-10-07 11:25:19 -0500

    WIP: snapshot local prep before play-mode integration (configs + uproject + train)

--- Content/Python/Train.py
+++ Content/Python/Train.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description='Train MA-POCA Agent')
     parser.add_argument('--config', type=str, default='Configs/TerraShift.json',
                         help='Path to JSON config file.')
-    parser.add_argument('--resume_from_checkpoint', type=str, default="model_update_test.pth",
+    parser.add_argument('--resume_from_checkpoint', type=str, default="checkpoints\\model_update_2007_t2.pth",
                         help='Path to a saved model checkpoint (.pth) to resume training from.')
     args = parser.parse_args()
 
